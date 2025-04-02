0098b8b6c398274f39eaab80cb8c14f7b765cf24
Alex Shelkovskiy <alexshelkov@gmail.com>
2025-04-02 14:00:00 +0200

    Add info about no agents

--- packages/nextjs/app/constants.ts
+++ packages/nextjs/app/constants.ts
@@ -1,9 +1,13 @@
 import img1 from './img/avatar1.webp';
+import img2 from './img/avatar2.png';
 
 export const agentsSettings = [
   {
     lang: 'en',
     image: img1
+  },
+  {
+    lang: 'en',
+    image: img2
   }
 ];
-
--- packages/nextjs/app/talk/page.tsx
+++ packages/nextjs/app/talk/page.tsx
@@ -3,7 +3,10 @@ import { agentsSettings } from '../constants';
 import { getClient } from '../neuphonic';
 
 export default async function AgentsPage() {
-  const fetchedAgents = (await getClient().agents.list()).slice(0, agentsSettings.length);
+  const fetchedAgents = (await getClient().agents.list()).slice(
+    0,
+    agentsSettings.length
+  );
 
   const agents = fetchedAgents.map((fetchedAgent, index) => {
     return {
@@ -15,21 +18,29 @@ export default async function AgentsPage() {
   });
 
   return (
-    <div className='flex min-h-screen items-center justify-center'>
-      <div className={`grid grid-cols-${Math.min(fetchedAgents.length, 3)} gap-4`}>
-        {agents.map((agent) => {
-          return (
-            <AgentCard
-              key={agent.id}
-              id={agent.id}
-              name={agent.name}
-              lang={agent.lang}
-              greet={agent.greet}
-              image={agent.image}
-            />
-          );
-        })}
-      </div>
+    <div className="flex min-h-screen items-center justify-center">
+      {agents.length ? (
+        <div
+          className={`grid grid-cols-${Math.min(fetchedAgents.length, 3)} gap-4`}
+        >
+          {agents.map((agent) => {
+            return (
+              <AgentCard
+                key={agent.id}
+                id={agent.id}
+                name={agent.name}
+                lang={agent.lang}
+                greet={agent.greet}
+                image={agent.image}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          No agents created yet, please use Neuphonic dashboard to create some.
+        </div>
+      )}
     </div>
   );
 }
