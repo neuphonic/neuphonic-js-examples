58ef9a30c936a6a47a8e137599d031ef8a93078f
Alex Shelkovskiy <alexshelkov@gmail.com>
2025-09-08 19:37:31 +0100

    Update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
